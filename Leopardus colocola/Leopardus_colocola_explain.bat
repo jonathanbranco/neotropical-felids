56af44e876fe6ec8c7933697f7e288bcf84087f5
Jonathan M. Branco <99761941+jonathanbranco@users.noreply.github.com>
2023-01-31 01:16:45 -0300

    Added L. colocola

---
+++ Leopardus colocola/Leopardus_colocola_explain.bat
@@ -0,0 +1,2 @@
+java -mx2713m -cp "/U:/My Drive/Research data/Felid distribution mapping/maxent/maxent.jar;U:\My Drive\Research data\Felid distribution mapping\maxent\maxent.jar" density.Explain -l "U:\My Drive\Research data\Felid distribution mapping\New distribution data\output\Leopardus colocola\Leopardus_colocola.lambdas" -c "U:\My Drive\Research data\Felid distribution mapping\New distribution data\output\Leopardus colocola\Leopardus_colocola.asc" "U:\My Drive\Research data\Felid distribution mapping\New distribution data\cropped layers"
+@if errorlevel 1 pause
