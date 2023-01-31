2d9a9af6ae8a69962241c72249c1f1a5c7369cae
Jonathan M. Branco <99761941+jonathanbranco@users.noreply.github.com>
2023-01-31 01:59:48 -0300

    Added L. tigrinus

---
+++ Leopardus tigrinus/Leopardus_tigrinus_explain.bat
@@ -0,0 +1,2 @@
+java -mx2713m -cp "/U:/My Drive/Research data/Felid distribution mapping/maxent/maxent.jar;U:\My Drive\Research data\Felid distribution mapping\maxent\maxent.jar" density.Explain -l "U:\My Drive\Research data\Felid distribution mapping\New distribution data\output\Leopardus tigrinus\Leopardus_tigrinus.lambdas" -c "U:\My Drive\Research data\Felid distribution mapping\New distribution data\output\Leopardus tigrinus\Leopardus_tigrinus.asc" "U:\My Drive\Research data\Felid distribution mapping\New distribution data\cropped layers"
+@if errorlevel 1 pause
